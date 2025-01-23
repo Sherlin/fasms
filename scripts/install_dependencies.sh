af33b47d882c3e718c95315df778cb6adc3d25e4
Sherlin <mun_huiyi@hotmail.sg>
2025-01-23 12:29:26 +0800

    Create directory

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -5,12 +5,3 @@ mkdir -p /home/ec2-user/frontend
 mkdir -p /home/ec2-user/backend
 sudo yum install -y nodejs npm golang
 
-# Install frontend dependencies
-cd /home/ec2-user/frontend
-npm install
-npm run build
-
-
-# Ensure backend dependencies are ready (Go modules)
-cd /home/ec2-user/backend
-go mod tidy
--- scripts/setup_services.sh
+++ scripts/setup_services.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Install frontend dependencies
+cd /home/ec2-user/frontend
+npm install
+npm run build
+
+
+# Ensure backend dependencies are ready (Go modules)
+cd /home/ec2-user/backend
+go mod tidy
+
 # Create backend systemd service
 cat <<EOF | sudo tee /etc/systemd/system/backend.service
 [Unit]
