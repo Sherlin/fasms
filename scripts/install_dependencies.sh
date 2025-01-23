9a1d79168c7abb2da47e220b431c2d3e328eb5a5
Sherlin <mun_huiyi@hotmail.sg>
2025-01-23 12:26:22 +0800

    Create directory

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # Update and install Node.js, npm, and Go
 #sudo apt-get update -y
-#sudo apt-get install -y nodejs npm golang
+mkdir -p /home/ec2-user/frontend
+mkdir -p /home/ec2-user/backend
+sudo yum install -y nodejs npm golang
 
 # Install frontend dependencies
 cd /home/ec2-user/frontend
