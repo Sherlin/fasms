979315ef04029c8641bf8bec2c1ecda7e4ce6469
Sherlin <mun_huiyi@hotmail.sg>
2025-02-03 12:00:18 +0800

    -Add base folder

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,5 +4,6 @@
 mkdir -p /home/ec2-user/frontend
 mkdir -p /home/ec2-user/backend
 sudo yum install -y nodejs npm golang nginx
+mkdir -p /var/www/html/
 cp -r /home/ec2-user/frontend/build /var/www/html/
 
