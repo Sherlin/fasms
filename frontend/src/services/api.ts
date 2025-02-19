5e0860934b08e41aa8e48bf1a4b2ff53fb92e54a
Sherlin <mun_huiyi@hotmail.sg>
2025-02-19 10:40:50 +0800

    - update URL

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8080/api",
+    baseURL: "https://fasms.moms-confession.com:8080/api",
 });
 
 export interface Applicant {
